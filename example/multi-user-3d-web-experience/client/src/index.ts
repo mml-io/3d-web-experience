099563bb1f3d89d79ba4b755b11d7f96e5b4cbf2
Sacha Morgese <sachamorgese@gmail.com>
2024-07-01 18:20:08 +0100

    Several UI tweaks

--- example/multi-user-3d-web-experience/client/src/index.ts
+++ example/multi-user-3d-web-experience/client/src/index.ts
@@ -28,8 +28,105 @@ const app = new Networked3dWebExperienceClient(holder, {
   mmlDocuments: [{ url: `${protocol}//${host}/mml-documents/example-mml.html` }],
   environmentConfiguration: {},
   avatarConfig: {
-    allowCustomAvatars: true,
-  },
+    availableAvatars: [
+      {
+        thumbnailUrl:"https://e7.pngegg.com/pngimages/799/987/png-clipart-computer-icons-avatar-icon-design-avatar-heroes-computer-wallpaper-thumbnail.png",
+        mmlCharacterUrl: "https://mmlstorage.com/eYIAFx/1706889930376.html",
+        name: "Avatar 1ahlishdflkjhaskjdfhlakj;shdfijlkhakjsdhfghjkabhsdyhuifbglakjhsdbfkjhabnsdkjfhnbsakjdhnbfjkws",
+        isDefaultAvatar: false,
+      },
+      {
+        thumbnailUrl: "https://models.readyplayer.me/65a8dba831b23abb4f401bae.png",
+        meshFileUrl: "https://models.readyplayer.me/65a8dba831b23abb4f401bae.glb",
+        name: "Avatar 2",
+        isDefaultAvatar: true,
+      },
+      {
+        thumbnailUrl: "https://static.vecteezy.com/system/resources/previews/019/896/008/original/male-user-avatar-icon-in-flat-design-style-person-signs-illustration-png.png",
+        mmlCharacterString: "<m-character src=\"https://mmlstorage.com/fca2e81688f8c26b1671b701e399f0a5c9756307607d78c11739293d2e530e78\">\n" +
+          "</m-character>",
+        name: "Avatar 3",
+        isDefaultAvatar: false,
+      },
+      {
+        thumbnailUrl:"https://e7.pngegg.com/pngimages/799/987/png-clipart-computer-icons-avatar-icon-design-avatar-heroes-computer-wallpaper-thumbnail.png",
+        mmlCharacterUrl: "https://mmlstorage.com/eYIAFx/1706889930376.html",
+        name: "Avatar 1",
+        isDefaultAvatar: false,
+      },
+      {
+        thumbnailUrl: "https://models.readyplayer.me/65a8dba831b23abb4f401bae.png",
+        meshFileUrl: "https://models.readyplayer.me/65a8dba831b23abb4f401bae.glb",
+        name: "Avatar 2",
+        isDefaultAvatar: true,
+      },
+      {
+        thumbnailUrl: "https://static.vecteezy.com/system/resources/previews/019/896/008/original/male-user-avatar-icon-in-flat-design-style-person-signs-illustration-png.png",
+        mmlCharacterString: "<m-character src=\"https://mmlstorage.com/fca2e81688f8c26b1671b701e399f0a5c9756307607d78c11739293d2e530e78\">\n" +
+          "</m-character>",
+        name: "Avatar 3",
+        isDefaultAvatar: false,
+      },
+      {
+        thumbnailUrl:"https://e7.pngegg.com/pngimages/799/987/png-clipart-computer-icons-avatar-icon-design-avatar-heroes-computer-wallpaper-thumbnail.png",
+        mmlCharacterUrl: "https://mmlstorage.com/eYIAFx/1706889930376.html",
+        name: "Avatar 1",
+        isDefaultAvatar: false,
+      },
+      {
+        thumbnailUrl: "https://models.readyplayer.me/65a8dba831b23abb4f401bae.png",
+        meshFileUrl: "https://models.readyplayer.me/65a8dba831b23abb4f401bae.glb",
+        name: "Avatar 2",
+        isDefaultAvatar: true,
+      },
+      {
+        thumbnailUrl: "https://static.vecteezy.com/system/resources/previews/019/896/008/original/male-user-avatar-icon-in-flat-design-style-person-signs-illustration-png.png",
+        mmlCharacterString: "<m-character src=\"https://mmlstorage.com/fca2e81688f8c26b1671b701e399f0a5c9756307607d78c11739293d2e530e78\">\n" +
+          "</m-character>",
+        name: "Avatar 3",
+        isDefaultAvatar: false,
+      },
+      {
+        thumbnailUrl:"https://e7.pngegg.com/pngimages/799/987/png-clipart-computer-icons-avatar-icon-design-avatar-heroes-computer-wallpaper-thumbnail.png",
+        mmlCharacterUrl: "https://mmlstorage.com/eYIAFx/1706889930376.html",
+        name: "Avatar 1",
+        isDefaultAvatar: false,
+      },
+      {
+        thumbnailUrl: "https://models.readyplayer.me/65a8dba831b23abb4f401bae.png",
+        meshFileUrl: "https://models.readyplayer.me/65a8dba831b23abb4f401bae.glb",
+        name: "Avatar 2",
+        isDefaultAvatar: true,
+      },
+      {
+        thumbnailUrl: "https://static.vecteezy.com/system/resources/previews/019/896/008/original/male-user-avatar-icon-in-flat-design-style-person-signs-illustration-png.png",
+        mmlCharacterString: "<m-character src=\"https://mmlstorage.com/fca2e81688f8c26b1671b701e399f0a5c9756307607d78c11739293d2e530e78\">\n" +
+          "</m-character>",
+        name: "Avatar 3",
+        isDefaultAvatar: false,
+      },
+      {
+        thumbnailUrl:"https://e7.pngegg.com/pngimages/799/987/png-clipart-computer-icons-avatar-icon-design-avatar-heroes-computer-wallpaper-thumbnail.png",
+        mmlCharacterUrl: "https://mmlstorage.com/eYIAFx/1706889930376.html",
+        name: "Avatar 1",
+        isDefaultAvatar: false,
+      },
+      {
+        thumbnailUrl: "https://models.readyplayer.me/65a8dba831b23abb4f401bae.png",
+        meshFileUrl: "https://models.readyplayer.me/65a8dba831b23abb4f401bae.glb",
+        name: "Avatar 2",
+        isDefaultAvatar: true,
+      },
+      {
+        thumbnailUrl: "https://static.vecteezy.com/system/resources/previews/019/896/008/original/male-user-avatar-icon-in-flat-design-style-person-signs-illustration-png.png",
+        mmlCharacterString: "<m-character src=\"https://mmlstorage.com/fca2e81688f8c26b1671b701e399f0a5c9756307607d78c11739293d2e530e78\">\n" +
+          "</m-character>",
+        name: "Avatar 3",
+        isDefaultAvatar: false,
+      },
+    ],
+    // allowCustomAvatars: true,
+  }
 });
 
 app.update();
