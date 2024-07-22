a61a90c0f770bc9b2969cbfb867d6a4ed98c31cd
TheCodeTherapy <me@mgz.me>
2024-07-22 12:32:05 +0100

    increases maximum fog distance

--- packages/3d-web-client-core/src/tweakpane/blades/environmentFolder.ts
+++ packages/3d-web-client-core/src/tweakpane/blades/environmentFolder.ts
@@ -50,7 +50,7 @@ const envOptions = {
   },
   fog: {
     fogNear: { min: 0, max: 80, step: 1 },
-    fogFar: { min: 81, max: 300, step: 1 },
+    fogFar: { min: 81, max: 1000, step: 1 },
   },
 };
 
