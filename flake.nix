762aa8460f13059152a4025737a0500bfee27ada
Daniel J. Rollins <me@deej.io>
2024-07-29 16:05:59 +0100

    add Nix flake for developer shell

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "MML 3D WebExperience Development Flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells = {
+          default = pkgs.mkShell { buildInputs = [ pkgs.nodejs_20 ]; };
+        };
+      }
+    );
+}
