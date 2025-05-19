179e0af5fa9265eb72c14e0574f2b9007d38e903
TheCodeTherapy <me@mgz.me>
2025-05-19 16:21:51 +0100

    fixes remote character lerping

--- packages/3d-web-client-core/src/character/RemoteController.ts
+++ packages/3d-web-client-core/src/character/RemoteController.ts
@@ -12,45 +12,49 @@ const tempQuaternion = new Quat();
 
 export class RemoteController {
   public currentAnimation: AnimationState = AnimationState.idle;
-
   public networkState: CharacterState;
 
+  private hasReceivedInitialUpdate = false;
+
   constructor(private config: RemoteControllerConfig) {
+    const pos = this.config.character.getPosition();
+    const rot = this.config.character.getRotation();
+
     this.networkState = {
       id: this.config.id,
-      position: {
-        x: this.config.character.getPosition().x,
-        y: this.config.character.getPosition().y,
-        z: this.config.character.getPosition().z,
-      },
-      rotation: {
-        quaternionY: tempQuaternion.setFromEulerXYZ(this.config.character.getRotation()).y,
-        quaternionW: 1,
-      },
-      state: this.currentAnimation as AnimationState,
+      position: { x: pos.x, y: pos.y, z: pos.z },
+      rotation: { quaternionY: rot.y, quaternionW: 1 },
+      state: this.currentAnimation,
     };
   }
 
   public update(clientUpdate: CharacterState, time: number, deltaTime: number): void {
-    if (!this.config.character) {
-      return;
-    }
-    this.updateFromNetwork(clientUpdate);
+    if (!this.config.character) return;
+    this.updateFromNetwork(clientUpdate, deltaTime);
     this.config.character.update(time, deltaTime);
   }
 
-  private updateFromNetwork(clientUpdate: CharacterState): void {
+  private updateFromNetwork(clientUpdate: CharacterState, deltaTime: number): void {
     const { position, rotation, state } = clientUpdate;
 
-    const characterPosition = new Vect3(this.config.character.getPosition());
-    const distanceSquared = characterPosition.distanceToSquared(characterPosition);
+    const currentPos = new Vect3(this.config.character.getPosition());
+    const targetPos = new Vect3(position.x, position.y, position.z);
 
-    if (distanceSquared > 5 * 5) {
-      // More than 5m of movement in a tick - the character is likely teleporting rather than just moving quickly - snap to the new position
-      this.config.character.setPosition(position.x, position.y, position.z);
+    if (!this.hasReceivedInitialUpdate) {
+      // First update, snap into position
+      this.config.character.setPosition(targetPos.x, targetPos.y, targetPos.z);
+      this.hasReceivedInitialUpdate = true;
     } else {
-      // TODO - lerp
-      this.config.character.setPosition(position.x, position.y, position.z);
+      const distSq = currentPos.distanceToSquared(targetPos);
+      // More than 5m of movement in a tick
+      // the character is likely teleporting rather than just moving quickly
+      // snap to the new position
+      if (distSq > 5 * 5) {
+        this.config.character.setPosition(targetPos.x, targetPos.y, targetPos.z);
+      } else {
+        const interpolatedPos = currentPos.lerp(targetPos, deltaTime);
+        this.config.character.setPosition(interpolatedPos.x, interpolatedPos.y, interpolatedPos.z);
+      }
     }
 
     const rotationQuaternion = tempQuaternion.set(0, rotation.quaternionY, 0, rotation.quaternionW);
