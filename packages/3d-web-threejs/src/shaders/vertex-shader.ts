706ecfd4778bb397d7bfa928c21e5c105b88ff23
TheCodeTherapy <me@mgz.me>
2026-01-06 11:30:31 +0000

    fixes broken post-processing

--- packages/3d-web-threejs/src/shaders/vertex-shader.ts
+++ packages/3d-web-threejs/src/shaders/vertex-shader.ts
@@ -4,6 +4,6 @@ export const vertexShader = /* glsl */ `
     out vec2 vUv;
     void main() {
       vUv = uv;
-      gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+      gl_Position = vec4(position, 1.0);
     }
   `;
